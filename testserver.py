036a0dd068a9cf22b7ba1b7a5e7828d6a51d26a0
joshuaevenson <joshuaevenson@gmail.com>
2012-02-04 01:18:37 -0800

    webserver import added for database, uncommented some stuff

--- testserver.py
+++ testserver.py
@@ -7,7 +7,7 @@ from webserver import Server
 
 class TestServer(unittest.TestCase):
 
-    def test_GET1(self):
+    def test_GET(self):
         headers = {'subscriptions' : json.dumps(['a', 'b']), 'location' : json.dumps('123')}
         request = Mock()
         request.getHeaders = Mock(return_value=headers)
@@ -15,10 +15,7 @@ class TestServer(unittest.TestCase):
 
         s = Server()
         
-        print "GET1 result:", s.render_GET(request)
-        
-
-
+        print "GET result:", s.render_GET(request)
 
 if __name__ == '__main__':
     unittest.main()
